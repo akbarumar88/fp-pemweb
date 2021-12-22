180b358dfee6f7e9f503978185d02f4e150ba68d
Akbar Umar Alfaro <akbarumar88@gmail.com>
2021-12-22 14:11:28 +0700

    Perbaiki layouting agar responsive dengan baik

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -1,10 +1,10 @@
 <h3 class="mb-4">Latest Aired</h3>
 
-<div class="row row-cols-sm-5">
+<div class="row row-cols-lg-5 row-cols-md-4 row-cols-sm-3 row-cols-2">
     <?php foreach ($movies as $movie) :  ?>
-        <div class="col-sm pr-2 pl-2">
+        <div class="col px-2 mb-4">
             <a href="">
-                <img width="100%" style="height:350px !important" src="<?= base_url('img/thumbnails/'.$movie['id'].'.jpg') ?>" class="img-thumbnail rounded" alt="...">
+                <img width="100%" style="height:300px !important" src="<?= base_url('img/thumbnails/'.$movie['id'].'.jpg') ?>" class="img-thumbnail rounded" alt="...">
                 <h6 class="text-center"><?= limit($movie['judul'], 7) ?></h6>
             </a>
         </div>
